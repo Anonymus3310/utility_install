18e0dbf07df770dabdcb4dd1a9d69dd303588b02
kushagradhi797 <62886323+kushagradhi797@users.noreply.github.com>
2020-04-28 20:36:45 +0530

    Update install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+pkg install nano git python python2 php openssh curl lolcat ruby
+git clone https://github.com/TechnicalMujeeb/tmvenom
+git clone https://github.com/thelinuxchoice/shellphish
+git clone git clone https://github.com/noob-hackers/grabcam
+
+
+pkg install unstable-repo
+pkg install metasploit
+git clone https://github.com/rapid7/metasploit-framework
+
